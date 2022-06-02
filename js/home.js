cd2403485faad69d67f284eb59fc639275eba30f
Tendooh <esaftler@pdx.edu>
2022-06-02 08:22:40 -0700

    updated hover effects

--- js/home.js
+++ js/home.js
@@ -1,14 +1,19 @@
 $(function() {
     $('#ethan').hover(function() {
-      $('#mepic').attr('src','../images/me-color.png');
+      $('#mepic').attr('src','../images/me-color6.png');
     }, function() {
       // on mouseout, reset the background colour
       $('#mepic').attr('src', '../images/me.png');
     });
   });
 
-// $('').hover(function(){
-// 	$(this).attr('src','http://mygimptutorial.com/images/button-with-metal-ring/8.jpg');
-// },function(){
-// 	$(this).attr('src','http://mygimptutorial.com/images/button-with-metal-ring/13.jpg');
-// });
+
+  $(function() {
+    $('#plang').hover(function() {
+      $('.lang').css('color','#d3a445');
+      $('.bar').css('color','white');
+    }, function() {
+      $('.lang').css('color', 'black');
+      $('.bar').css('color', 'black');
+    });
+  });
