8e74d40adfe1e5903feac298bda7f6e0d45016aa
Tendooh <esaftler@pdx.edu>
2022-06-03 01:04:30 -0700

    added project

--- js/project.js
+++ js/project.js
@@ -1,3 +1,15 @@
+$color0 = 'black',
+$color1  = 'red',
+$color2  = 'orange',
+$color3  = 'yellow',
+$color4  = 'green',
+$color5  = 'blue',
+$color6  = 'purple',
+$color7  = 'violet',
+$color8  = 'white',
+$color9  = 'gray',
+$colorx = 'pink',
+
 $(document).ready(function (){
   for(let i = 0; i < 5000; i++){
     $('.pagebox')
@@ -11,12 +23,14 @@ $(document).ready(function (){
       justifyContent: 'center',
       backgroundColor: 'rgb(200, 155, 100)',
       gridTemplateColumns: 'repeat(50, 1fr)',
-      gridTempalteRows: 'repeat(100, 1fr)'
+      gridTempalteRows: 'repeat(100, 1fr)',
+      borderStyle: 'solid',
+      borderColor: '#d3a445'
     })
     .append(
       $('<div>')
       .attr({
-        class: 'gbox',
+        class: 'gabox',
       })
       .css({
         width: '.5em',
@@ -31,13 +45,14 @@ $(document).ready(function (){
       })
       .on('mouseover', function(){
         $(this).css({
-          backgroundColor: 'rgb(100, 155, 220)',
+          backgroundColor: $colorx,
         });
       })
       .on('mouseout', function(){
         console.log('left', $(this).data('item'));
       })
     )
+    
 
   $('#input')
   .css({
@@ -52,5 +67,166 @@ $(document).ready(function (){
     console.log('mouse over event')
   })
   }
+
+  for(let i = 0; i < 10; i++){
+    $('.input')
+    .css({
+      maxWidth: '400px',
+      maxHeight: '800px',
+      display: 'grid',
+      margin: '0 auto',
+      padding: '0',
+      justifyContent: 'center',
+      backgroundColor: 'rgb(200, 155, 100)',
+      gridTemplateColumns: 'repeat(10, 1fr)',
+      gridTempalteRows: '1fr',
+      borderStyle: 'solid',
+      borderColor: '#d3a445'
+    })
+    .append(
+      $('<div>')
+      .attr({
+        class: 'gbox gbox'+i,
+      })
+      .css({
+        width: '2.5em',
+        height: '2.5em',
+      })
+      .data({
+        btn: i,
+      })
+    )
+    if(i == 0){
+      $('.gbox'+i)
+      .css({
+        backgroundColor: $color1,
+      })
+      .on('click', function(){
+        $colorx = $color1
+      })
+    }
+    if(i == 1){
+      $('.gbox'+i)
+      .css({
+        backgroundColor: $color2,
+      })
+      .on('click', function(){
+        $colorx = $color2
+      })
+    }
+    if(i == 2){
+      $('.gbox'+i)
+      .css({
+        backgroundColor: $color3,
+      })
+      .on('click', function(){
+        $colorx = $color3
+      })
+    }
+    if(i == 3){
+      $('.gbox'+i)
+      .css({
+        backgroundColor: $color4,
+      })
+      .on('click', function(){
+        $colorx = $color4
+      })
+    }
+    if(i == 4){
+      $('.gbox'+i)
+      .css({
+        backgroundColor: $color5,
+      })
+      .on('click', function(){
+        $colorx = $color5
+      })
+    }
+    if(i == 5){
+      $('.gbox'+i)
+      .css({
+        backgroundColor: $color6,
+      })
+      .on('click', function(){
+        $colorx = $color6
+      })
+    }
+    if(i == 6){
+      $('.gbox'+i)
+      .css({
+        backgroundColor: $color7,
+      })
+      .on('click', function(){
+        $colorx = $color7
+      })
+    }
+    if(i == 7){
+      $('.gbox'+i)
+      .css({
+        backgroundColor: $color8,
+      })
+      .on('click', function(){
+        $colorx = $color8
+      })
+    }
+    if(i == 8){
+      $('.gbox'+i)
+      .css({
+        backgroundColor: $color9,
+      })
+      .on('click', function(){
+        $colorx = $color9
+      })
+    }
+    if(i == 9){
+      $('.gbox'+i)
+      .css({
+        backgroundColor: $color0,
+      })
+      .on('click', function(){
+        $colorx = $color0
+      })
+    }
+  }
+  $('.btn')
+  .css({
+    maxWidth: '100px',
+    maxHeight: '25px',
+    margin: '0 auto',
+    padding: '0',
+    justifyContent: 'center',
+    backgroundColor: 'pink',
+  })
+  $('.btn #reset')
+  .css({
+    width: '100px',
+    height: '25px',
+    visibility: 'visible'
+  })
+  .on('click', function(){
+    $('.gabox').css({backgroundColor: 'white'})
+  })
 })
 
+
+
+$(function() {
+  $('.gbox').hover(function() {
+    $(this)
+    .css(
+      'filter', 'brightness(85%)',
+      );
+  }, function() {
+    // on mouseout, reset the background colour
+    $(this)
+    .css(
+      'filter', 'brightness(100%)',
+      );
+  });
+});
+
+
+function hideLoader() {
+  $('#loader').hide();
+}
+$(window).ready(hideLoader);
+setTimeout(hideLoader, 10 * 1000);  
