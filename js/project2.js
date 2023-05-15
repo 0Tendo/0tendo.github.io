d6d0d1a04b3c5b6fb5eae8b640e378cbca91561a
0Tendo <esaftler@gmail.com>
2023-05-14 19:55:11 -0700

    added nilakantha series

--- js/project2.js
+++ js/project2.js
@@ -1,4 +1,5 @@
 $(document).ready(function (){
+  Decimal.set({ precision: 600 });
   $('#calculate1').on('click', function(){
       let pi = 4;
       let i = 0;
@@ -34,7 +35,51 @@ $(document).ready(function (){
         }
     }, 100);
 });
+
+$('#calculate3').on('click', function(){
+  let iterations = $('#iterations3').val();
+  
+  if(iterations > 50) {
+    alert("Maximum iterations for the Chudnovsky formula is 50.");
+    return;
+  }
+  
+  let pi = calculatePiChudnovsky(iterations);
+  $('#result3').text(pi.toString());
 });
+
+function factorial(n) {
+  let fact = new Decimal(1);
+  for(let i = 1; i <= n; i++) {
+      fact = fact.times(i);
+  }
+  return fact;
+}
+
+function calculatePiChudnovsky(iterations) {
+  let pi = new Decimal(0);
+  let twelve = new Decimal(12);
+  let one = new Decimal(1);
+  let minusOne = new Decimal(-1);
+  let two = new Decimal(2);
+  let three = new Decimal(3);
+  let six = new Decimal(6);
+  let constant = new Decimal(13591409);
+  let multiplier = new Decimal(545140134);
+  let divisor = new Decimal(640320);
+
+  for (let k = 0; k < iterations; k++) {
+      let firstTerm = factorial(6 * k).times(multiplier.times(k).plus(constant));
+      let secondTerm = factorial(3 * k).times(factorial(k).toPower(3)).times(divisor.toPower(3 * k + 1.5)); // Notice the 3*k+1.5 in the toPower()
+      let thirdTerm = minusOne.toPower(k);
+      pi = pi.plus(firstTerm.div(secondTerm).times(thirdTerm));
+  }
+
+  pi = one.div(pi.times(twelve));
+  return pi;
+}
+});
+
 
 function hideLoader() {
 $('#loader').hide();
