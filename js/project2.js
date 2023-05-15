464fc585f3281f3ac0cc421b4a55ec5c3af68f64
0Tendo <esaftler@gmail.com>
2023-05-14 18:27:23 -0700

    updated pi calc

--- js/project2.js
+++ js/project2.js
@@ -1,30 +1,27 @@
-
 $(document).ready(function (){
-    $('.btn').on('click', function calcPi(){
-        let pi = 4;
-    for (let i = 0; i < 50000; i++)
-    {
-        
-      let denom = i*2 +3;
-      if (i % 2 == 0)
-      {
-          pi -= (4/denom);
-      }else{
-          pi += (4/denom);
-      }
-      console.log(pi);
-      $('.input').text(pi);
-    }
-    // hideLoader();
-    })
-})
+  $('#calculate').on('click', function(){
+      let pi = 4;
+      let i = 0;
+      let intervalId = setInterval(function() {
+        let denom = i*2 +3;
+        if (i % 2 == 0)
+        {
+            pi -= (4/denom);
+        }else{
+            pi += (4/denom);
+        }
+        console.log(pi);
+        $('#result').text(pi);
+        i++;
+        if(i >= $('#iterations').val()) {
+          clearInterval(intervalId);
+        }
+      }, 100);
+  });
+});
 
-
-  function hideLoader() {
-    $('#loader').hide();
-  }
-  $(window).ready(hideLoader);
-  setTimeout(hideLoader, 10 * 1000);  
-
-
-  
+function hideLoader() {
+$('#loader').hide();
+}
+$(window).ready(hideLoader);
+setTimeout(hideLoader, 10 * 1000);
