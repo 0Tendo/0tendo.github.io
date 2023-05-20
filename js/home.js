833a7ed66d900b3e014aa0e8dc608a452f18bd7c
0Tendo <esaftler@gmail.com>
2023-05-20 15:46:40 -0700

    updated resume toggle

--- js/home.js
+++ js/home.js
@@ -19,101 +19,30 @@ $(function() {
   });
 
 
-$(function() {
-  $('#psu')
-    .css({
-    })
-    .append(
-        $('<p>')
-        .text('Field: Computer Science (Undergraduate Bachelors)')
-        .css({
-            fontSize: '16px',
-            color: 'white',
-        }))
-        .append(
-            $('<p>')
-            .text('Years: 2019-Present')
-            .css({
-                fontSize: '16px',
-                color: 'white',
-            }))
-            .append(
-                $('<p>')
-                .text('Expected Graduation: Spring 2023')
-                .css({
-                    fontSize: '16px',
-                    color: 'white',
-                }))
-        
-  })
-  
-$(function() {
-    $('#lam')
-      .css({
-      })
-      .append(
-          $('<p>')
-          .text('Job: Lead Engineering Technician')
-          .css({
-              fontSize: '16px',
-              color: 'white',
-          }))
-          .append(
-              $('<p>')
-              .text('Department: Pilot Development and Manufactuing')
-              .css({
-                  fontSize: '16px',
-                  color: 'white',
-              }))
-              .append(
-                  $('<p>')
-                  .text('Years: 2018-Present')
-                  .css({
-                      fontSize: '16px',
-                      color: 'white',
-                  }))
-                  .append(
-                    $('<p>')
-                    .text('Skills: Team Management | Excel | Root Cause Analysis | Quality Inspections | Networking | Electrical and Mechanical Troubleshooting')
-                    .css({
-                        fontSize: '16px',
-                        color: 'white',
-                    }))
-          
-    })
-
-
   $(function() {
-    $('#navy')
-      .css({
-      })
-      .append(
-          $('<p>')
-          .text('Rate: Engineering Laboratory Technician (Nuclear) | Workcenter Supervisor')
-          .css({
-              fontSize: '16px',
-              color: 'white',
-          }))
-          .append(
-              $('<p>')
-              .text('Boat: USS Oklahoma City (Fast Attack Submarine)')
-              .css({
-                  fontSize: '16px',
-                  color: 'white',
-              }))
-              .append(
-                  $('<p>')
-                  .text('Years: 2010-2016')
-                  .css({
-                      fontSize: '16px',
-                      color: 'white',
-                  }))
-                  .append(
-                    $('<p>')
-                    .text('Skills: Critical Maintenance Planning | Data Analysis | Water Chemistry Analysis | Radiation Controls | Mechanical and Electrical System Operations and Maintenance')
-                    .css({
-                        fontSize: '16px',
-                        color: 'white',
-                    }))
-          
-    })
+    $('#psu').click(function() {
+      $(this).find('p').toggle();
+    }).append(
+      $('<p>').text('Field: Computer Science (Undergraduate Bachelors)').css({fontSize: '16px', color: 'white', display: 'none'}),
+      $('<p>').text('Years: 2019-Present').css({fontSize: '16px', color: 'white', display: 'none'}),
+      $('<p>').text('Expected Graduation: Spring 2023').css({fontSize: '16px', color: 'white', display: 'none'})
+    );
+  
+    $('#lam').click(function() {
+      $(this).find('p').toggle();
+    }).append(
+      $('<p>').text('Job: Lead Engineering Technician').css({fontSize: '16px', color: 'white', display: 'none'}),
+      $('<p>').text('Department: Pilot Development and Manufactuing').css({fontSize: '16px', color: 'white', display: 'none'}),
+      $('<p>').text('Years: 2018-Present').css({fontSize: '16px', color: 'white', display: 'none'}),
+      $('<p>').text('Skills: Team Management | Excel | Root Cause Analysis | Quality Inspections | Networking | Electrical and Mechanical Troubleshooting').css({fontSize: '16px', color: 'white', display: 'none'})
+    );
+  
+    $('#navy').click(function() {
+      $(this).find('p').toggle();
+    }).append(
+      $('<p>').text('Rate: Engineering Laboratory Technician (Nuclear) | Workcenter Supervisor').css({fontSize: '16px', color: 'white', display: 'none'}),
+      $('<p>').text('Boat: USS Oklahoma City (Fast Attack Submarine)').css({fontSize: '16px', color: 'white', display: 'none'}),
+      $('<p>').text('Years: 2010-2016').css({fontSize: '16px', color: 'white', display: 'none'}),
+      $('<p>').text('Skills: Critical Maintenance Planning | Data Analysis | Water Chemistry Analysis | Radiation Controls | Mechanical and Electrical System Operations and Maintenance').css({fontSize: '16px', color: 'white', display: 'none'})
+    );
+  });
