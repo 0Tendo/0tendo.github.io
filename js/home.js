30a006655e967ede3c99061c76fbbcabad523144
0Tendo <esaftler@gmail.com>
2023-05-21 19:09:35 -0700

    updated resume

--- js/home.js
+++ js/home.js
@@ -12,10 +12,10 @@ $(function() {
   $(function() {
     $('#plang').hover(function() {
       $('.lang').css('color','#d3a445');
-      $('.bar').css('color','white');
+      $('.bar').css('color','#C0C0C0');
     }, function() {
-      $('.lang').css('color', 'black');
-      $('.bar').css('color', 'black');
+      $('.lang').css('color', '#C0C0C0');
+      $('.bar').css('color', '#C0C0C0');
     });
   });
   */
@@ -23,28 +23,31 @@ $(function() {
 
   $(function() {
     $('#psu').click(function() {
-      $(this).find('p').toggle();
+        $(this).find('p').toggle();
     }).append(
-      $('<p>').text('Field: Computer Science (Undergraduate Bachelors)').css({fontSize: '16px', color: 'white', display: 'none'}),
-      $('<p>').text('Years: 2019-Present').css({fontSize: '16px', color: 'white', display: 'none'}),
-      $('<p>').text('Expected Graduation: Spring 2023').css({fontSize: '16px', color: 'white', display: 'none'})
+        $('<p>').text('2019-Present').css({fontSize: '18px', color: 'black'}),
+        $('<p>').text('Computer Science (B.S)').css({fontSize: '18px', color: '#C0C0C0'}),
+        $('<p>').text('Expected Graduation: December 2023').css({fontSize: '18px', color: '#C0C0C0'}),
+        $('<p>').html('AI <span class="bar">|</span> Machine Learning <span class="bar">|</span>Networking <span class="bar">|</span> SQL').css({fontSize: '18px', color: '#C0C0C0'})
     );
-  
+
     $('#lam').click(function() {
-      $(this).find('p').toggle();
+        $(this).find('p').toggle();
     }).append(
-      $('<p>').text('Job: Lead Engineering Technician').css({fontSize: '16px', color: 'white', display: 'none'}),
-      $('<p>').text('Department: Pilot Development and Manufactuing').css({fontSize: '16px', color: 'white', display: 'none'}),
-      $('<p>').text('Years: 2018-Present').css({fontSize: '16px', color: 'white', display: 'none'}),
-      $('<p>').text('Skills: Team Management | Excel | Root Cause Analysis | Quality Inspections | Networking | Electrical and Mechanical Troubleshooting').css({fontSize: '16px', color: 'white', display: 'none'})
+        $('<p>').text('2018-Present').css({fontSize: '18px', color: 'black'}),
+        $('<p>').text('Lead Engineering Technician').css({fontSize: '18px', color: '#C0C0C0'}),
+        $('<p>').text('Department: Pilot Manufactuing').css({fontSize: '18px', color: '#C0C0C0'}),
+        $('<p>').html('Quality Inspections <span class="bar">|</span> Electrical & Mechanical Troubleshooting').css({fontSize: '18px', color: '#C0C0C0'}),
+        $('<p>').html('Communication <span class="bar">|</span> Team Management <span class="bar">|</span> Excel <span class="bar">|</span> SQL <span class="bar">|</span> Root Cause Analysis ').css({fontSize: '18px', color: '#C0C0C0'})
     );
-  
+
     $('#navy').click(function() {
-      $(this).find('p').toggle();
+        $(this).find('p').toggle();
     }).append(
-      $('<p>').text('Rate: Engineering Laboratory Technician (Nuclear) | Workcenter Supervisor').css({fontSize: '16px', color: 'white', display: 'none'}),
-      $('<p>').text('Boat: USS Oklahoma City (Fast Attack Submarine)').css({fontSize: '16px', color: 'white', display: 'none'}),
-      $('<p>').text('Years: 2010-2016').css({fontSize: '16px', color: 'white', display: 'none'}),
-      $('<p>').text('Skills: Critical Maintenance Planning | Data Analysis | Water Chemistry Analysis | Radiation Controls | Mechanical and Electrical System Operations and Maintenance').css({fontSize: '16px', color: 'white', display: 'none'})
+        $('<p>').text('2010-2016').css({fontSize: '18px', color: 'black'}),
+        $('<p>').text('US Navy Nuke').css({fontSize: '18px', color: '#C0C0C0'}),
+        $('<p>').text('Engineering Laboratory Technician').css({fontSize: '18px', color: '#C0C0C0'}),
+        $('<p>').html('Radiation Safety <span class="bar">|</span> Reactor Operation <span class="bar">|</span> Water Chemistry Analysis <span class="bar">|</span> Work Center Supervisor').css({fontSize: '18px', color: '#C0C0C0'}),
+        $('<p>').html('Teamwork <span class="bar">|</span> Leadership <span class="bar">|</span> Data Analysis <span class="bar">|</span> Nuclear Chemistry and Physics <span class="bar">|</span> Lab Skills').css({fontSize: '18px', color: '#C0C0C0'})
     );
-  });
+});
